9e542d566bfa80d624b9e3231156f48f8556a5b0
Diego <pelaezdiegoalejo@hotmail.com>
2019-01-17 09:52:08 -0500

    utilisation de TraiteFichier

---
+++ PelaezAutoPartsDiego/PelaezAutoPartsDiego/Utility/TraiteFichierUsager.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace PelaezAutoPartsDiego.Utility
+{
+
+    public class TraiteFichierUsager
+    {
+        string line;
+        char[] charParametreSeparator = new char[] { ',' };
+        char[] charSeparatorsLine = new char[] { ';' };
+        string[] UsagerPass;
+        int contador = 0;
+        StreamReader ObjecReader;
+
+
+        //Read file Usagers.txt -return collection avec Nom et pas registres dans le fichier
+        public  string[] splitLineUsager() {
+            
+            ObjecReader = File.OpenText("U:\\Projet\\Usagers.txt");
+            try
+            {
+                string line = ObjecReader.ReadLine();
+
+                while (line != null)
+                {
+
+                    //contador =+ 1;
+                    // string[] readUsagerComplete = line.Split(charSeparatorsLine);
+                    // string[] splitUsager = readUsagerComplete[0].Split(charParametreSeparator);
+                    string[] splitUsager = line.Split(charParametreSeparator);
+                    UsagerPass[contador] = splitUsager[2].ToString();
+                    UsagerPass[contador+1] = splitUsager[3].ToString();
+                    UsagerPass[contador+2] = splitUsager[0].ToString();
+                    contador =+ 3;
+                     line = ObjecReader.ReadLine();
+                }
+            }
+            catch(FileNotFoundException e){
+           
+
+            }
+            finally{
+                ObjecReader.Close();
+            }
+            return UsagerPass;  
+        }
+        public string[]GetInfoCompleteUsager (string id)
+        {
+            bool retrouve = false;
+            string[] Usager = new string[8];
+            ObjecReader = File.OpenText("U:\\Projet\\Usagers.txt");
+            string line = ObjecReader.ReadLine();
+            try
+            {
+                while (retrouve == false && line != null) 
+                {
+                    Usager = line.Split(charParametreSeparator); 
+                    if(Usager[0].Trim().Equals(id))
+                    {
+                        retrouve = true;
+                        return Usager;
+                    }
+                    else
+                    {
+                        line = ObjecReader.ReadLine();
+                    }
+                }
+            }
+            catch(FileNotFoundException e)
+            {
+
+            }
+            finally
+            {
+                ObjecReader.Close();
+            }
+            return Usager;
+        }
+
+    }
+}
--- PelaezAutoPartsDiego/PelaezAutoPartsDiego/Utility/Validation.cs
+++ PelaezAutoPartsDiego/PelaezAutoPartsDiego/Utility/Validation.cs
@@ -1,4 +1,5 @@
 ﻿using PelaezAutoPartsDiego.Models;
+using System.IO;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,30 +10,62 @@ namespace PelaezAutoPartsDiego.Utility
     public class Validation
     {
 
-        public static Usager ValidationUsager (Usager usager)
+        public static Usager ValidationUsager (Usager usagerCourant)
         {
-            //Usager de Exj
-            Usager UsagerModel = new Usager
-            {
-                Id = 1,
-                Nom = "Diego Alejandro Pelaez",
-                UsagerP = "diego",
-                Password = "123",
-                Telephone = "5115455555",
-                Adresse = "4455 Avnoot",
-                CodePostal = "Q2Q5T5",
-                TypeUsager = "ADMIN",
+            string[] UsagerEtPass;
+            string[] InfoUsagerCourant; 
 
-            };
-            if ((UsagerModel.UsagerP.Equals(usager.UsagerP)) && (UsagerModel.Password.Equals(usager.Password)))
+            TraiteFichierUsager line = new TraiteFichierUsager();
+
+            UsagerEtPass = line.splitLineUsager();
+            if (usagerCourant.UsagerP.Equals(UsagerEtPass[0]) && (usagerCourant.Password.Equals(UsagerEtPass[1])))
             {
-                return UsagerModel;
+                InfoUsagerCourant = line.GetInfoCompleteUsager(UsagerEtPass[3]);
+                Usager UsagerCourant = new Usager
+                {
+                    Id = int.Parse(InfoUsagerCourant[0]),
+                    Nom = InfoUsagerCourant[1],
+                    UsagerP = InfoUsagerCourant[2],
+                    Password = InfoUsagerCourant[3],
+                    Telephone = InfoUsagerCourant[4],
+                    Adresse = InfoUsagerCourant[5],
+                    CodePostal = InfoUsagerCourant[6],
+                    TypeUsager = InfoUsagerCourant[7],
+                };
+                return usagerCourant;
             }
-            else
-            {
-                return null;
-            } 
-                       
+
+            return usagerCourant;
+
+
+
+
+            ////Creation d'objet Usager 
+            //Usager UsagerModel = new Usager
+            //{
+            //    Id = 1,
+            //    Nom = "Diego Alejandro Pelaez",
+            //    UsagerP = "diego",
+            //    Password = "123",
+            //    Telephone = "5115455555",
+            //    Adresse = "4455 Avnoot",
+            //    CodePostal = "Q2Q5T5",
+            //    TypeUsager = "ADMIN",
+
+            //};
+
+
+
+            ////Verification de usager et mot de passe 
+            //if ((UsagerModel.UsagerP.Equals(usagerCourant.UsagerP)) && (UsagerModel.Password.Equals(usagerCourant.Password)))
+            //{
+            //    return UsagerModel;
+            //}
+            //else
+            //{
+            //    return null;
+            // } 
+
         }
     }
 }
